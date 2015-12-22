bb2531927c197656165d03d6745714e441bc313d
Harald Kraemer <tetha42@gmail.com>
2015-12-22 17:13:52 +0100

    some rework

--- plant/__main__.py
+++ plant/__main__.py
@@ -4,6 +4,23 @@ import pygame
 
 import plant.main
 
+class GameContainer(object):
+    def __init__(self):
+        self.eventbus = EventBus()
+        self.display_parts = []
+        self.actors = []
+
+    def draw_all(self, screen):
+        for display_part in self.display_parts:
+            display_part.draw(screen)
+
+    def add_display_part(self, display_part):
+        self.display_parts.append(display_part)
+
+    def act_all(self, delta):
+        for actor in self.actors:
+            actor.act(delta)
+
 class EventBus(object):
     def __init__(self):
         self.subscribers = collections.defaultdict(list)
@@ -29,71 +46,68 @@ class EventPrinter(object):
             print "Event %s -> %s" % (name, data)
 
 class GridDisplay(object):
-    def __init__(self, eventbus):
-        self.eventbus = eventbus
+    def __init__(self, game):
+        self.game = game
 
-        self.eventbus.register("grid.created", self.create_grid_display)
+        self.game.eventbus.register("grid.created", self.create_grid_display)
 
     def create_grid_display(self, event_name, grid):
         for r in xrange(0, grid.height):
             for c in xrange(0, grid.width):
-                CellDisplay(self.eventbus, r, c, 100*r, 100*c)
+                self.game.add_display_part(CellDisplay(r, c, 100*r, 100*c))
 
 class CellDisplay(object):
-    def __init__(self, eventbus, row, col, x, y):
+    def __init__(self, row, col, x, y):
         self.row = row
         self.col = col
 
         self.x = x
         self.y = y
 
-        self.eventbus = eventbus
-        self.eventbus.register("draw", self.draw)
-        self.eventbus.register("grid.created", self.create_grid)
-
     def create_grid(self, event_name, grid):
         self.grid = grid
 
-    def draw(self, event_name, screen):
+    def draw(self, screen):
         rect = pygame.Rect(self.x+5, self.y+5, 90, 90)
         screen.fill((0, 255, 0), rect)
         
 class Grid(object):
-    def __init__(self, eventbus):
-        self.eventbus = eventbus
+    def __init__(self, game):
+        self.game = game
+
         self.width = 5
         self.height = 5
         self.grid = collections.defaultdict(None)
 
-        self.eventbus.register("boot", self.create_grid)
+        self.game.eventbus.register("boot", self.create_grid)
 
     def create_grid(self, event_name, event_data):
-        self.eventbus.fire("grid.created", self)
+        self.game.eventbus.fire("grid.created", self)
 
     def __repr__(self):
         return "Grid(width=%d,height=%d,grid=%s)" % (self.width, self.height, self.grid)
 
 def run():
-    eventbus = EventBus()
+    game = GameContainer()
     screen = pygame.display.set_mode((800, 600))
     clock = pygame.time.Clock()
 
     running = True
 
-    eventbus.register("", EventPrinter(ignore=["draw"]))
+    game.eventbus.register("", EventPrinter(ignore=["draw"]))
 
-    Grid(eventbus)
-    GridDisplay(eventbus)
+    Grid(game)
+    GridDisplay(game)
 
 
-    eventbus.fire("boot")
+    game.eventbus.fire("boot")
     while running:
         clock.tick(60)
         event = pygame.event.poll()
         if event.type == pygame.QUIT:
             running = False
         screen.fill((0, 0, 0))
-        eventbus.fire("draw", screen)
+        game.draw_all(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
