13674ee5ee3a97b662f65ea8fd3d1aa1968019d3
Harald Kraemer <tetha42@gmail.com>
2015-12-24 15:41:04 +0100

    removed event bus

--- plant/__main__.py
+++ plant/__main__.py
@@ -8,49 +8,67 @@ import plant.main
 
 class GameContainer(object):
     def __init__(self):
-        self.eventbus = EventBus()
-        self.display_parts = []
-        self.actors = []
+        self.entities = EntitySet()
+
+    def all_with(self, *tags):
+        return self.entities.all_with(*tags)
+
+    def add_entity(self, entity):
+        self.entities.add(entity)
 
     def draw_all(self, screen):
-        for display_part in self.display_parts:
-            display_part.draw(screen)
-
-    def add_display_part(self, display_part):
-        self.display_parts.append(display_part)
+        for entity in self.all_with('draw'):
+            entity.draw(screen)
 
     def act_all(self, delta):
-        for actor in self.actors:
+        for entity in self.all_with('act_per_frame'):
             actor.act(delta)
 
-class EventBus(object):
-    def __init__(self):
-        self.subscribers = collections.defaultdict(list)
+class EntitySet(object):
+    def __init__(self, entities=None):
+        if entities is None:
+            entities = []
+        self.entities = entities
 
-    def register(self, event_name, listener):
-        self.subscribers[event_name].append(listener)
+    def add(self, entity):
+        self.entities.append(entity)
 
-    def fire(self, event_name, *args, **kwargs):
-        current_subs = dict(self.subscribers)
-        for k, v in current_subs.iteritems():
-            if event_name.startswith(k):
-                for sub in v:
-                    sub.__call__(event_name, *args, **kwargs)
+    def all_with(self, *tags):
+        return EntitySet([e for e in self.entities if all(tag in e.tags for tag in tags)])
+
+    def call(self, callback):
+        for e in self.entities:
+            callback(e)
+
+    def __getitem__(self, idx):
+        return self.entities[idx]
+
+    def __len__(self):
+        return len(self.entities)
 
 class GameElement(object):
     def __init__(self, game):
         self.game = game
+        self.tags = []
         self.display_part = None
 
-    def on_event(self, event_name, callback):
-        self.game.eventbus.register(event_name, callback)
+        self.game.add_entity(self)
 
-    def fire_event(self, event_name, *args, **kwargs):
-        self.game.eventbus.fire(event_name, *args, **kwargs)
+    def add_tag(self, tag):
+        self.tags.append(tag)
+
+    def draw(self, screen):
+        self.display_part.draw(screen)
+
+    def enable_clicks(self):
+        self.button = Button(self.game, self.clicked)
+
+    def clicked(self, button):
+        self.on_click.__call__(button)
 
     def add_placeholder_sprite(self):
+        self.add_tag('draw')
         self.display_part = PlaceHolderSprite(self.game)
-        self.game.add_display_part(self.display_part)
         
 
 class PlaceHolderSprite(object):
@@ -75,12 +93,6 @@ class PlaceHolderSprite(object):
     @property
     def rect(self):
         return pygame.Rect(self.x+self.margin/2, self.y+self.margin/2, self.width-self.margin/2, self.height-self.margin/2)
-    
-    def enable_clicks(self):
-        self.button = Button(self.game, self.rect, self.clicked)
-
-    def clicked(self, button):
-        self.on_click.__call__(button)
 
     def draw(self, screen):
         screen.fill((self.red, self.green, self.blue), self.rect)
@@ -104,22 +116,30 @@ class GridDisplay(GameElement):
     def __init__(self, game):
         super(GridDisplay, self).__init__(game)
 
-        self.on_event("grid.created", self.create_grid_display)
+        grid = game.all_with('grid')[0]
 
-    def create_grid_display(self, event_name, grid):
         for r in xrange(0, grid.height):
             for c in xrange(0, grid.width):
                 CellDisplay(self.game, r, c, 100*r, 100*c)
 
 class Button(GameElement):
-    def __init__(self, game, rect, on_click):
+    def __init__(self, game, on_click):
         super(Button, self).__init__(game)
-        self.rect = rect
+
+        self.x = 0
+        self.y = 0
+
+        self.width = 100
+        self.height = 100
+
         self.on_click = on_click
+        self.add_tag('click_observer')
 
-        self.on_event("input.mouse.click", self.handle_click)
+    @property
+    def rect(self):
+        return pygame.Rect(self.x, self.y, self.width, self.height)
 
-    def handle_click(self, event_name, button, position):
+    def mouse_click(self, button, position):
         if self.rect.collidepoint(position):
             self.on_click.__call__(button)
 
@@ -156,15 +176,22 @@ class CellDisplay(GameElement):
         self.display_part.margin = 10
         self.display_part.marker = "PLAINS"
 
-        self.display_part.enable_clicks()
-        self.display_part.on_click = self.clicked
+        self.enable_clicks()
+        self.button.x = x
+        self.button.y = y
+        self.button.width = 100
+        self.button.height = 100
+        self.button.on_click = self.clicked
 
-        self.on_event("grid.tile.%d.%d.updated" % (self.row, self.col), self.tile_updated)
+        self.add_tag('tile_observer')
 
     def clicked(self, button):
-        self.fire_event("tile_clicked", self.row, self.col)
+        self.game.all_with('grid').call(lambda e: e.change_tile(self.row, self.col))
 
-    def tile_updated(self, event_name, new_field_type):
+    def tile_updated(self, row, col, new_field_type):
+        if row != self.row or col != self.col:
+            return
+
         if new_field_type is None:
             self.display_part.marker = "PLAINS"
         else:
@@ -178,13 +205,12 @@ class Grid(GameElement):
         self.height = 5
         self.grid = {} 
 
-        self.on_event("boot", self.create_grid)
-        self.on_event("tile_clicked", self.change_tile)
+        self.add_tag('grid')
 
     def field_type(self, row, column):
         return self.grid.get((row, column), None)
 
-    def change_tile(self, event_name, row, column):
+    def change_tile(self, row, column):
         if self.field_type(row, column) is None:
             new_type = "TOWN"
         else:
@@ -197,10 +223,9 @@ class Grid(GameElement):
             del self.grid[(row, col)]
         else:
             self.grid[(row, col)] = new_type
-        self.fire_event("grid.tile.%d.%d.updated" % (row, col), new_type)        
 
-    def create_grid(self, event_name):
-        self.fire_event("grid.created", self)
+        for observer in self.game.all_with('tile_observer'):
+            observer.tile_updated(row, col, new_type)
 
     def __repr__(self):
         return "Grid(width=%d,height=%d,grid=%s)" % (self.width, self.height, self.grid)
@@ -214,20 +239,17 @@ def run():
 
     running = True
 
-    game.eventbus.register("", EventPrinter(ignore=["draw"]))
-
     Grid(game)
     GridDisplay(game)
     BuyLeafButton(game)
 
-    game.eventbus.fire("boot")
     while running:
         clock.tick(120)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.locals.MOUSEBUTTONUP:
-                game.eventbus.fire("input.mouse.click", event.button, event.pos)
+                game.all_with('click_observer').call(lambda e: e.mouse_click(event.button, event.pos))
         screen.fill((0, 0, 0))
         game.draw_all(screen)
         pygame.display.flip()
